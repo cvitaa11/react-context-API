407ea1128dbccca30b239a0ae38ebf6f9b8acf89
Ivan Cvitković <icvitkovic@combis.ba>
2020-03-25 11:38:10 +0100

    toggle as function

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export default class ToggleTheme extends Component {
-  static contextType = ThemeContext;
-
-  render() {
-    const { toggleTheme } = this.context;
-    return (
-      <button
-        onClick={toggleTheme}
-        style={{ borderRadius: 3, height: "2rem", cursor: "pointer" }}
-      >
-        Toggle theme
-      </button>
-    );
-  }
+export default function ToggleTheme() {
+  return (
+    <ThemeContext.Consumer>
+      {context => {
+        const { toggleTheme } = context;
+        return (
+          <button onClick={toggleTheme} className="toggleBtn">
+            Toggle theme
+          </button>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
 }
